d1512702dad32f658f64d709f6234630af51a284
yushan <yushanwebdev@gmail.com>
2022-06-04 10:15:20 +0530

    feat: Workcation Hero Development - Configure the custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          dark: "#0c87b8",
+          DEFAULT: "#0fa9e6",
+        },
+      },
+    },
   },
   plugins: [],
 };
